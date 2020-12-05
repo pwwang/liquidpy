e2c05f56f1aa5f945b3ccc99846ee1fb1b7d7ba5
pwwang <pwwang@pwwang.com>
2020-12-05 14:55:45 -0600

    0.6.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='liquidpy',
-    version='0.6.2',
+    version='0.6.3',
     description='A port of liquid template engine for python',
     python_requires='==3.*,>=3.6.0',
     project_urls={
