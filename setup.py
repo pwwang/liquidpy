ee48b19b1dc7962d8c1939c6aaae7511ecc1dca4
pwwang <pwwang@pwwang.com>
2021-08-28 18:46:39 -0700

    Require jinja2 3+

--- setup.py
+++ setup.py
@@ -34,6 +34,6 @@ setup(
     packages=['liquid', 'liquid.exts', 'liquid.filters', 'liquid.tags'],
     package_dir={"": "."},
     package_data={},
-    install_requires=['jinja2==2.*,>=2.5.0 || ==3.*,>=3.0.0'],
+    install_requires=['jinja2==3.*,>=3.0.0'],
     extras_require={"dev": ["pytest", "pytest-cov"]},
 )
