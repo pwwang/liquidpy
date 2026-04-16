d0b3f971e7a74bbfba6ebd2e5eb70fcb03e8839b
pwwang <pwwang@pwwang.com>
2026-04-15 23:44:19 -0700

    refactor: use uv instead of poetry

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 This will not be included in the distribution.
-The distribution is managed by poetry
+The distribution is managed by uv/hatchling.
 This file is kept only for
 1. Github to index the dependents
 2. pip install -e .
